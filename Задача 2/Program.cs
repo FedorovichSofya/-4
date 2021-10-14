8762682400df2906ad5952a4fc1cb59ee63c02f9
s.fedorovich <s.fedorovich@CZC71776GT>
2021-10-14 15:28:02 +0300

    задача 3

---
+++ Задача 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // сравнение количества положительных и отрицательных чисел       
+            int n = 1; // число положительных чисел
+            int k = 1; //число отрицательных чисел
+            int a;
+            Console.WriteLine("Введите последовательность целых чисел: ");
+            Console.WriteLine("Для окончания ввода последовательности введите ноль");
+              do
+            {
+                a = Convert.ToInt32(Console.ReadLine());
+                if (a > 0)
+                    n++;
+                else
+                    if (a != 0)
+                    k++;
+            }
+            while (a != 0);
+            if (n > k)
+                Console.WriteLine("Количество положительных чисел больше количества отрицательных");
+            else
+                if (n < k)
+                Console.WriteLine("Количество отрицательных чисел больше количества положительных");
+            else
+                Console.WriteLine("Количество положительных чисел равно количеству отрицательных");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задача 3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // найти количество квадратов на прямоугольнике
+            Console.Write("Введите A>0: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите B>0: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите C>0: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int k = 0;
+            int n = 0;
+            while (a >= c)
+            {
+                a -= c;
+                k++;
+            }
+            while (b >= c)
+            {
+                b -= c;
+                n++;
+            }
+            int square = 0;
+            for (int i = 0; i < k; i++)
+                square += n;
+            Console.WriteLine("Количество квадратов, размещенных в прямоугольнике равно " + square);
+            Console.ReadKey();
+        }
+    }
+}
